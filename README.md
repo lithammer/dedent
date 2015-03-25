02dd7207aa26858faeccb98749016744087cea15
Peter Renström <renstrom.peter@gmail.com>
2015-03-25 17:15:51 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,47 @@
+# Dedent
+
+Removes common leading whitespace from multiline strings. Inspired by [`textwrap.dedent`](https://docs.python.org/3/library/textwrap.html#textwrap.dedent) in Python.
+
+## Usage / example
+
+Imagine the following snippet that prints a multiline string. You want the indentation to both look nice in the code as well as in the actual output.
+
+```go
+package main
+
+import (
+    "fmt"
+
+    "github.com/renstrom/dedent"
+)
+
+func main() {
+    s := `Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.
+        Curabitur justo tellus, facilisis nec efficitur dictum,
+        fermentum vitae ligula. Sed eu convallis sapien.`
+    fmt.Println(dedent.Dedent(s))
+    fmt.Println("-------------")
+    fmt.Println(s)
+}
+```
+
+To illustrate the difference, here's the output:
+
+
+```bash
+$ go run main.go
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit.
+Curabitur justo tellus, facilisis nec efficitur dictum,
+fermentum vitae ligula. Sed eu convallis sapien.
+-------------
+Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.
+        Curabitur justo tellus, facilisis nec efficitur dictum,
+        fermentum vitae ligula. Sed eu convallis sapien.
+```
+
+## License
+
+MIT
---
+++ dedent_test.go
@@ -0,0 +1,51 @@
+package dedent
+
+import "testing"
+
+func TestFindMargin(t *testing.T) {
+	m := findMargin("Foo\n\t\t\tBar\n\t\tBaz")
+	if m != "\t\t" {
+		t.Errorf("Expected margin to be '\t\t'; got '%s'", m)
+	}
+}
+
+func TestFindMarginMultiline(t *testing.T) {
+	m := findMargin(`Foo
+			Bar
+		Baz
+`)
+	if m != "\t\t" {
+		t.Errorf("Expected margin to be '\t\t'; got '%s'", m)
+	}
+}
+
+func TestDedentMultilineString(t *testing.T) {
+	s1 := `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		Curabitur justo tellus, facilisis nec efficitur dictum,
+		fermentum vitae ligula. Sed eu convallis sapien.
+	`
+	s2 := `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Curabitur justo tellus, facilisis nec efficitur dictum,
+fermentum vitae ligula. Sed eu convallis sapien.
+`
+
+	if Dedent(s1) != s2 {
+		t.Errorf("expected string '%s'; got '%s'", s2, Dedent(s1))
+	}
+}
+
+func BenchmarkDedent(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Dedent(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		Curabitur justo tellus, facilisis nec efficitur dictum,
+		fermentum vitae ligula. Sed eu convallis sapien.`)
+	}
+}
+
+func BenchmarkFindMargin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		findMargin(`Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		Curabitur justo tellus, facilisis nec efficitur dictum,
+		fermentum vitae ligula. Sed eu convallis sapien.`)
+	}
+}
